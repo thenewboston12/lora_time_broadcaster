e80f7bbddcfc03b78bfce89b2c990c3662f0e228
Yermakhan Magzym <57003533+thenewboston12@users.noreply.github.com>
2022-11-06 01:36:43 +0600

    sf 12 update
    still guard time of the receiver is too large

--- gateway/main.py
+++ gateway/main.py
@@ -8,8 +8,10 @@ import pycom
 import time
 import utime
 import gc
-from machine import RTC
 import machine
+from machine import RTC
+from machine import Timer
+
 
 # saving energy by disabling all the networks
 bt = Bluetooth()
@@ -32,12 +34,12 @@ light_green = 0xdbe003
 purple = 0xb134eb
 
 # change this for appropriate WiFi access point
+#
+# _WIFI_SSID = "TP-Link_56B1"
+# _WIFI_PASS = "34134634"
 
-_WIFI_SSID = "TP-Link_56B1"
-_WIFI_PASS = "34134634"
-
-# _WIFI_SSID = "NU"
-# _WIFI_PASS = "1234512345"
+_WIFI_SSID = "NU"
+_WIFI_PASS = "1234512345"
 
 _NTP_URL = "pool.ntp.org"
 _TIMEZONE_OFFSET = 6* 60**2 # For GMT +6
@@ -48,7 +50,8 @@ _TIME_PERIOD_MS = 5000
 
 
 # set LoRa parameters
-lora = LoRa(mode=LoRa.LORA, region=LoRa.EU868, bandwidth=LoRa.BW_125KHZ)
+# Spreading Factor 12
+lora = LoRa(mode=LoRa.LORA, region=LoRa.EU868, bandwidth=LoRa.BW_125KHZ, sf=12)
 
 pycom.rgbled(white)
 
@@ -82,26 +85,44 @@ time.timezone(_TIMEZONE_OFFSET)
 s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
 s.setblocking(False)
 
-
+# chrono = Timer.Chrono()
 
 while True:
+    # print("1 loop took: " + str(chrono.read_ms()) + "ms")
+    # chrono.reset()
+    # chrono.start()
+
     pycom.rgbled(green)
 
     cur_time = time.localtime()
     print("Current time: ", cur_time)
 
     # get precise time
-    seconds = time.time() +1
+    seconds = time.time() + 1
 
     bytes = seconds.to_bytes(4, 'little', False)
 
-    # Just send the seconds
+    # Send the time
     s.send(bytes)
-    print("Sent the time")
+
+    # Measure Time On Air
+    #(rx_timestamp, rssi, snr, sftx, sfrx, tx_trials, tx_power, tx_time_on_air, tx_counter, tx_frequency)
+
+    print(lora.stats())
+    toa = lora.stats()[7]
+    rssi = lora.stats()[1]
+    snr = lora.stats()[2]
+    print("Time On Air: " + str(toa) + "ms")
+
+    print("RSSI: " + str(rssi)  )
+    print("SNR: " + str(snr) )
+
+
 
     pycom.rgbled(off)
 
     # Send sync time every _TIME_PERIOD_MS milliseconds
-    time.sleep_ms(_TIME_PERIOD_MS)
+    # 8ms is the time the loop runs
+    time.sleep_ms(_TIME_PERIOD_MS - 8)
 
 print("THE END")
